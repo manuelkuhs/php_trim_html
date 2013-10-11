f7ad395f6b9f9bdee97d2bf8a1c9c2b637af6183
manuelkuhs <manuelkuhs@gmail.com>
2013-10-11 15:28:32 +0100

    Update trim_html.php
    fixed typo

--- trim_html.php
+++ trim_html.php
@@ -1,6 +1,6 @@
 function trim_html($str,$trim_str,$html=true) {
     if ($html) {
-        $str = preg_replace('/((\s*<\/*\s*br\s*\/*\s*>\s*)+\z)|(\A(\s*<\/*\s*br\s*\/*\s*>\s*)+)/i' , '' , $str);
+        $str = preg_replace('/((\s*<\/*\s*$trim_str\s*\/*\s*>\s*)+\z)|(\A(\s*<\/*\s*$trim_str\s*\/*\s*>\s*)+)/i' , '' , $str);
     } 
     // set $html to false to only trim exactly $trim_str (i.e. won't apply any "<", "/>" etc.) 
     // Thus you can trim any regex - e.g. 
