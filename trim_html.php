409541ed1586d3d5f634013c51f698d8e637afec
manuelkuhs <manuelkuhs@gmail.com>
2013-10-11 15:56:39 +0100

    Update trim_html.php
    added ability to pass array of strings/HTML tags to trim

--- trim_html.php
+++ trim_html.php
@@ -1,12 +1,29 @@
 function trim_html($str,$trim_str,$html=true) {
+    // if $trim_str is an array, such as '("br", "blockquote")', build regex component
+    if (is_array($trim_str)) {
+        foreach ($trim_str as $el) {
+            if (!isset($i)) {
+                $trim_strs = "(" . $el;
+                $i = 1;
+            }
+            else {
+                $trim_strs = $trim_strs . "|" . $el;
+            }
+        }
+        $trim_strs = $trim_strs . ")";
+    }
+    else {
+        $trim_strs = $trim_str;
+    }
+    
+    // begin trimming
     if ($html) {
-        $str = preg_replace('/((\s*<\/*\s*$trim_str\s*\/*\s*>\s*)+\z)|(\A(\s*<\/*\s*$trim_str\s*\/*\s*>\s*)+)/i' , '' , $str);
+        $str = preg_replace('/((\s*<\/*\s*' . $trim_strs. '\s*\/*\s*>\s*)+\z)|(\A(\s*<\/*\s*' . $trim_strs. '\s*\/*\s*>\s*)+)/i' , '' , $str);
     } 
-    // set $html to false to only trim exactly $trim_str (i.e. won't apply any "<", "/>" etc.) 
-    // Thus you can trim any regex - e.g. 
+    // set $html to false to only trim exactly $trim_str (i.e. won't apply any "<", "/>" etc.) Thus you can trim any regex - e.g. 
     // setting $html false and $trim_str to "[a-z]" will trim all alphabetical characters
     else {
-        $str = preg_replace('/(\A\s*$trim_str+\s*)|(\s*$trim_str+\s*\z)/i' , '' , $str);
+        $str = preg_replace('/(\A\s*' . $trim_strs . '+\s*)|(\s*' . $trim_strs . '+\s*\z)/i' , '' , $str);
     }
     return $str;
 }
